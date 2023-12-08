a745a2351cd3c1f8941b606f860b03deba9952e1
Kopp Sebastian <sebastian.kopp@gmx.at>
2023-12-08 11:54:19 +0100

    fix the error bug and add a design

--- src/Components/MainLayout.jsx
+++ src/Components/MainLayout.jsx
@@ -1,33 +1,47 @@
 import { useState } from "react";
 import FetchData from "./FetchData";
 import ShowWeather from "./Showweather";
+import "../styles/stlye.css";
 
 const MainLayout = () => {
+  const [input, setInput] = useState(null);
   const [city, setCity] = useState(null);
   const [isShown, setIsShown] = useState(false);
-  const { temp, error } = FetchData(
+
+  const { temp } = FetchData(
     `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=`
   );
 
   const handleInput = (event) => {
-    setCity(event.target.value);
+    setInput(event.target.value);
   };
 
   const compareIsShown = () => {
     isShown === false ? setIsShown(true) : setIsShown(false);
+    setCity(null);
+  };
+
+  const changeCity = () => {
+    setCity(input);
   };
 
   return (
     <div className="container d-flex justify-content-center align-items-center vh-100">
       {!isShown && (
-        <div className="d-flex flex-column justify-content-center align-items-center border border-primary p-4">
+        <div className="d-flex flex-column justify-content-center align-items-center">
           <h1>How is the Weather in</h1>
           <input
             className="form-control text-center mb-2"
             type="text"
             onChange={handleInput}
           />
-          <button className="btn btn-primary" onClick={compareIsShown}>
+          <button
+            className="btn test"
+            onClick={() => {
+              compareIsShown();
+              changeCity();
+            }}
+          >
             Go
           </button>
         </div>
@@ -35,7 +49,6 @@ const MainLayout = () => {
       {isShown && (
         <ShowWeather temp={temp} city={city} compareIsShown={compareIsShown} />
       )}
-      {error && <h1>{error}</h1>}
     </div>
   );
 };
--- src/Components/Showweather.jsx
+++ src/Components/Showweather.jsx
@@ -7,10 +7,10 @@ const ShowWeather = (props) => {
 
   return (
     <div className="container d-flex justify-content-center align-items-center vh-100">
-      <div className="d-flex flex-column justify-content-center align-items-center border border-primary p-4">
+      <div className="d-flex flex-column justify-content-center align-items-center">
         <h1>The Weather in {props.city} is </h1>
-        <p>{props.temp} degrees</p>
-        <button className="btn btn-primary" onClick={handleClickButton}>
+        <p className="pa">{props.temp} degrees</p>
+        <button className="btn test" onClick={handleClickButton}>
           New Request
         </button>
       </div>
